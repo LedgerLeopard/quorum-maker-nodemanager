46e31c6fde7653949969d8e95e026a45245c81fa
Timofey Bobkov <bobkov.tim@gmail.com>
2020-01-17 09:21:54 +0300

    fix

--- NetworkManagerContract.sol
+++ NetworkManagerContract.sol
@@ -1,11 +1,11 @@
-pragma solidity^0.5.0;
+pragma solidity^0.6.1;
 contract NetworkManagerContract {
 
     uint nodeCounter;
-  
-    struct NodeDetails {  
+
+    struct NodeDetails {
         string nodeName;
-        string role; 
+        string role;
         string publickey;
         string enode;
         string ip;
@@ -14,7 +14,7 @@ contract NetworkManagerContract {
 
     mapping (string => NodeDetails)nodes;
     string[] enodeList;
-    
+
     event print(string nodeName, string role,string publickey, string enode, string ip, string id);
 
     function registerNode(string memory n, string memory r, string memory p, string memory e, string memory ip, string memory id) public {
@@ -26,9 +26,9 @@ contract NetworkManagerContract {
         nodes[e].id = id;
         enodeList.push(e);
         emit print(n, r, p, e, ip, id);
-    
+
     }
-   
+
     function getNodeDetails(uint16 _index) public view returns (string memory n, string memory r, string memory p, string memory ip, string memory e, string memory id, uint i) {
         NodeDetails memory nodeInfo = nodes[enodeList[_index]];
         return (
@@ -57,7 +57,7 @@ contract NetworkManagerContract {
     }
 
     function getNodeList(uint16 i)  public  view   returns (string memory n, string memory r,string memory p,string memory ip,string memory e, string memory id) {
-        
+
         NodeDetails memory nodeInfo = nodes[enodeList[i]];
         return (
             nodeInfo.nodeName,
